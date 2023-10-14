8f2a43202e61dd505558cdcbb558732e76da1573
reza72ahmadi <reza72.ahmadi@gmail.com>
2023-10-14 22:57:44 +0430

    Added had to many association from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params) 
-
+    @article.user = User.first
     if @article.save
       flash[:notice] = 'Article successfuly created'
       redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_171424) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_175524) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
