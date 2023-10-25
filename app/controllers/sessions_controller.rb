b96d20c6ba96ddd35b71d8fd82d03b3e5125b5a3
reza72ahmadi <reza72.ahmadi@gmail.com>
2023-10-25 16:36:25 +0430

    sessions2  added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:notice] = "Logged in successfully"
+        redirect_to user
+        else
+            flash.now[:alert] = "There was something wrong with your login detaile"
+            render 'new'
+        end
     end
 
     def destroy
