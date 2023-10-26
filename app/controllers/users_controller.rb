e97d3a1bd709d18797fb0ab9295e48a1fb373c83
reza72ahmadi <reza72.ahmadi@gmail.com>
2023-10-27 00:19:22 +0430

    admin can manipulate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user
         flash[:notice] = "Acount deldeted"
         redirect_to articles_path
     end
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
         flash[:alert] = "You can only edit or delete your own profile"
         redirect_to @user
         end
